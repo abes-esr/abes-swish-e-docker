714e0d47142d7f333ddd13a3fa91735b2df80960
mickael.seror@gmail.com <mickael.seror@gmail.com>
2021-09-13 11:35:26 +0200

    .swishcgi.conf path changed

--- scripts/dockerRunGm-swish.sh
+++ scripts/dockerRunGm-swish.sh
@@ -4,6 +4,6 @@
 docker run --restart always -d --name gm-swish  \
 -p 8080:80  \
 -v /var/www/html/guide/html/:/var/www/html/ \
--v /var/www/html/guide/html/.swishcgi.conf:/usr/lib/cgi-bin/.swishcgi.conf \
+-v /opt/guide/data-swish-e/.swishcgi.conf:/usr/lib/cgi-bin/.swishcgi.conf \
 -v /opt/guide/data-swish-e/:/opt/guide/data-swish-e/ \
 mylocalswish-e:latest
