0d9704501c116866d4f7ac45fb297aa477ed83cc
Stéphane Gully <gully@abes.fr>
2021-09-22 16:28:06 +0200

    on injecte swish.cgi et TemplateDefault.pm qui sont personnalisés pour l'Abes

--- scripts/dockerRunGm-swish.sh
+++ scripts/dockerRunGm-swish.sh
@@ -4,6 +4,8 @@
 docker run --restart always -d --name gm-swish  \
 -p 8080:80  \
 -v /var/www/html/guide/html/:/var/www/html/ \
+-v /opt/guide/data-swish-e/swish.cgi:/usr/lib/cgi-bin/swish.cgi \
+-v /opt/guide/data-swish-e/TemplateDefault.pm:/usr/lib/swish-e/perl/SWISH/TemplateDefault.pm \
 -v /opt/guide/data-swish-e/.swishcgi.conf:/usr/lib/cgi-bin/.swishcgi.conf \
 -v /opt/guide/data-swish-e/:/opt/guide/data-swish-e/ \
 mylocalswish-e:latest
