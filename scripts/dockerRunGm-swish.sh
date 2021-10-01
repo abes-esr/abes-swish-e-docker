60140e0c355f492ba720b013220c1fe1728f3b20
Mike2git <88394707+mike2git@users.noreply.github.com>
2021-10-01 15:28:36 +0200

    change the version number : 1.1.1

--- scripts/dockerRunGm-swish.sh
+++ scripts/dockerRunGm-swish.sh
@@ -8,4 +8,4 @@ docker run --restart unless-stopped -d --name gm-swish  \
 -v /opt/guide/data-swish-e/TemplateDefault.pm:/usr/lib/swish-e/perl/SWISH/TemplateDefault.pm \
 -v /opt/guide/data-swish-e/.swishcgi.conf:/usr/lib/cgi-bin/.swishcgi.conf \
 -v /opt/guide/data-swish-e/:/opt/guide/data-swish-e/ \
-abesesr/swish-e-docker:1.1.0
+abesesr/swish-e-docker:1.1.1
